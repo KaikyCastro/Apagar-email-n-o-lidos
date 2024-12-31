5ffdaee62d7ecdebf275ba67770717ecedbfc18c
Kaiky Castro <kaiky.castro7@hotmail.com>
2024-12-31 16:44:34 -0300

    Versão final

--- Apagar email não lidos/Apagar_email_não_lidos.py
+++ Apagar email não lidos/Apagar_email_não_lidos.py
@@ -1,2 +1,21 @@
-
 import pyautogui
+from time import sleep
+import keyboard
+
+while True:
+    # Verificar se a tecla ESC foi pressionada para interromper o loop
+    if keyboard.is_pressed("esc"):
+        print("Loop interrompido pelo usuario!")
+        break
+
+    # Clicar na caixinha de expansão
+    pyautogui.click(375, 217, duration=0.5)
+    sleep(0.5)
+
+    # Selecionar os emails não lidos
+    pyautogui.click(469, 374, duration=0.5)
+    sleep(0.5)
+
+    # Apagar os emails não lidos
+    pyautogui.click(536, 221, duration=0.5)
+    sleep(0.5)
